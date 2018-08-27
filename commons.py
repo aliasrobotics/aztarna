9c2c2a2e1b47bbbcd669bc5bbaa3f9c78a9b3a29
Gorka Olalde Mendia <gorka.olalde@alumni.mondragon.edu>
2018-08-27 09:54:46 +0200

    Added common classes for both scanners

---
+++ commons.py
@@ -0,0 +1,45 @@
+class BaseScanner:
+
+    def __init__(self, net_range, port, extended):
+        self.net_range = net_range
+        self.port = port
+        self.extended = extended
+
+    def scan(self):
+        raise NotImplementedError
+
+    def print_results(self):
+        raise NotImplementedError
+
+    def write_to_file(self, out_file):
+        raise NotImplementedError
+
+
+
+class BaseHost:
+    def __init__(self):
+        self.address = ''
+        self.port = ''
+        self.nodes = []
+
+class BaseNode:
+    def __init__(self):
+        self.name = ''
+        self.address = ''
+        self.port = ''
+
+class Topic:
+    def __init__(self):
+        self.name = ''
+        self.type = ''
+
+
+class Service:
+    def __init__(self):
+        self.name = ''
+
+class Parameter:
+    def __init__(self):
+        self.name = ''
+        self.type = ''
+        self.value = ''
