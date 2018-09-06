3aea1839acaa4d6bb17f3bd60027d734817f450d
Xabier PB <xpb@Xabiers-MacBook-Pro.local>
2018-09-06 16:52:25 +0200

    Fix typo

--- aztarna/ros/scanner.py
+++ aztarna/ros/scanner.py
@@ -62,7 +62,7 @@ class ROSScanner(BaseScanner):
 
                         await self.set_xmlrpcuri_node(ros_master_client, ros_host)
                         await client.close()
-                        self.logger.warning('[+] ROS Host found at {}!!!'.format(ros_host))
+                        self.logger.warning('[+] ROS Host found at {}!!!'.format(ros_host.address))
                     else:
                         self.logger.critical('[-] Error getting system state. Probably not a ROS Master')
 
