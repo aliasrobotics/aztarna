62a3d2fed279b3375ada10b97d6d9f3ca1a27df7
Xabier PB <xpb@Xabiers-MacBook-Pro.local>
2018-09-26 11:08:51 +0200

    Add piping example script to incorporate in aztarna

---
+++ pipe_stdout.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+
+# use stdin if it's full                                                        
+if not sys.stdin.isatty():
+    input_stream = sys.stdin
+
+# otherwise, read the given filename                                            
+else:
+    try:
+        input_filename = sys.argv[1]
+    except IndexError:
+        message = 'need filename as first argument if stdin is not full'
+        raise IndexError(message)
+    else:
+        input_stream = open(input_filename, 'rU')
+
+for line in input_stream:
+    print line # do something useful with each line
