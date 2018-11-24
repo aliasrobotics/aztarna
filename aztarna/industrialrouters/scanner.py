f79ba83e7c414f37d44ecea79365ce180946991d
ahcorde <ahcorde@gmail.com>
2018-11-24 18:16:03 +0100

    Adding westermo routes

--- aztarna/industrialrouters/scanner.py
+++ aztarna/industrialrouters/scanner.py
@@ -18,6 +18,8 @@ class IndustrialRouterScanner(BaseScanner):
         self.hosts_eWON_secure = []
         self.hosts_moxa = []
         self.hosts_moxa_secure = []
+        self.hosts_westermo = []
+        self.hosts_westermo_secure = []
 
         self.logger = logging.getLogger(__name__)
 
@@ -37,6 +39,28 @@ class IndustrialRouterScanner(BaseScanner):
         :param address: address of the industrial router
         :param port: port of the industrial router
         """
+        auth = aiohttp.BasicAuth(login='admin', password='westermo')
+        async with aiohttp.ClientSession(auth=auth, loop=asyncio.get_event_loop(), timeout=self.timeout) as client:
+            full_host = 'http://' + str(address) + ':' + str(port)
+            try:
+                async with client.get(full_host) as response:
+                    async def fetch(client, host):
+                        async with client.get(host) as resp:
+                            assert resp.status == 200
+                            return await resp.text()
+                    server = response.headers.get('WWW-Authenticate')
+                    if server==None:
+                        return
+                    if "Westermo" in server:
+                        if response.status != 401:
+                            self.hosts_westermo.append(full_host)
+                            return
+                        else:
+                            self.hosts_westermo_secure.append(full_host)
+                            return
+            except Exception as e:
+                self.logger.error('[-] Error connecting to Westermo host ' + str(full_host) + " " + str(e) + '\n')
+            await client.close()
 
         async with aiohttp.ClientSession(loop=asyncio.get_event_loop(), timeout=self.timeout) as client:
             full_host = 'http://' + str(address) + ':' + str(port)
@@ -123,6 +147,10 @@ class IndustrialRouterScanner(BaseScanner):
             print("Moxa router in " + host + " is secure")
         for host in self.hosts_moxa:
             print("MOXA router in " + host + " is not secure")
+        for host in self.hosts_westermo_secure:
+            print("Westermo router in " + host + " is secure")
+        for host in self.hosts_westermo:
+            print("Westermo router in " + host + " is not secure")
 
 
     def scan(self):
