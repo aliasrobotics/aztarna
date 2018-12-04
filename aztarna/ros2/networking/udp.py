5d7bf0005234c44082db4789f8d523ba2b54521f
Gorka Olalde Mendia <gorka.olalde@alumni.mondragon.edu>
2018-12-04 15:09:21 +0100

    Remove ROS2

--- aztarna/ros2/networking/udp.py
+++
@@ -1,8 +0,0 @@
-import struct
-
-
-class UDP:
-
-    def __init__(self, raw_data):
-        self.src_port, self.dest_port, self.size = struct.unpack('! H H 2x H', raw_data[:8])
-        self.data = raw_data[8:]
