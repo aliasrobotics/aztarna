e2b2cc4e38f0ab6a538091839f52debff48cc7aa
Gorka Olalde Mendia <gorka.olalde@alumni.mondragon.edu>
2018-12-06 21:37:14 +0100

    Updated Moxa Router scanner

--- aztarna/industrialrouters/scanner.py
+++ aztarna/industrialrouters/scanner.py
@@ -1,13 +1,21 @@
 import asyncio
-from asyncio import ALL_COMPLETED
+import logging
+import random
+import re
+import ssl
+from asyncio import ALL_COMPLETED, Semaphore
+from asyncio import TimeoutError
 from typing import List
 
 import aiohttp
 from aiohttp import ClientTimeout
 from colorama import Fore
+from ipwhois import IPWhois
 from shodan.client import Shodan
 
 from aztarna.commons import RobotAdapter
+
+logger = logging.getLogger(__name__)
 
 
 class BaseIndustrialRouter:
@@ -18,6 +26,8 @@ class BaseIndustrialRouter:
         self.valid_credentials = []
         self.alive = False
         self.protocol = None
+        self.country = ''
+        self.asn_description = ''
 
 
 class EWonRouter(BaseIndustrialRouter):
@@ -56,45 +66,115 @@ class BaseIndustrialRouterScanner:
                     router = cls.router_cls()
                     router.address = result['ip_str']
                     router.port = result['port']
+                    if result['_shodan']['module'] == 'http-simple-new':
+                        router.protocol = 'http'
+                    elif result['_shodan']['module'] == 'https-simple-new':
+                        router.protocol = 'https'
+                    else:
+                        router.protocol = result['_shodan']['module']
+
                     router.protocol = result['_shodan']['module']
                     found_routers.append(router)
+        logger.info('[+] Shodan found {} routers. Scanner: {}'.format(len(found_routers), cls.__name__))
         return found_routers
 
     @classmethod
-    async def check_is_router(cls, address: str, port: int) -> bool:
-        async with aiohttp.ClientSession(timeout=ClientTimeout(0.5)) as client:
-            uri = 'http://{}:{}'.format(address, port)
-            async with client.get(uri) as response:
-                for field, values in cls.possible_headers:
-                    if response.headers.get(field) in values:
-                        return True
-                    else:
-                        return False
+    async def check_is_router(cls, address: str, port: int, semaphore=Semaphore()) -> BaseIndustrialRouter:
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        context.options &= ~ssl.OP_NO_SSLv3
+        async with semaphore:
+            async with aiohttp.ClientSession(timeout=ClientTimeout(2)) as client:
+                uri = 'http://{}:{}'.format(address, port)
+                print('[+] Connecting to {}'.format(address))
+                async with client.get(uri, ssl=context) as response:
+                    for field, values in cls.possible_headers:
+                        if response.headers.get(field) in values:
+                            router = cls.router_cls()
+                            router.address = address
+                            router.port = port
+                            return router
+                        else:
+                            return None
+
+    def check_routers(self, addresses, ports):
+        async def check_routers_aio(addresses, ports):
+            semaphore = Semaphore(100)
+            futures = []
+            routers = []
+            for address in addresses:
+                for port in ports:
+                    futures.append(asyncio.ensure_future(self.check_is_router(address, port, semaphore=semaphore)))
+            done, pending = await asyncio.wait(futures)
+            for future in done:
+                if future:
+                    routers.append(future)
+            return routers
+
+        return asyncio.run(check_routers_aio(addresses, ports), debug=True)
 
     @classmethod
-    async def check_default_password(cls, router):
+    async def check_default_password(cls, router, semaphore=Semaphore()):
         uri = '{}://{}:{}/{}'.format(router.protocol, router.address, router.port, cls.url_path)
-        for user, password in cls.default_credentials:
-            auth = aiohttp.BasicAuth(login=user, password=password)
-            async with aiohttp.ClientSession(timeout=ClientTimeout(2)) as client:
-                try:
-                    async with client.request('GET', uri, auth=auth, ssl=False) as response:
-                        if response.status == 200:
-                            router.valid_credentials.append((user, password))
-                        elif response.status == 401:
-                            pass
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        context.options &= ~ssl.OP_NO_SSLv3
+        async with semaphore:
+            for user, password in cls.default_credentials:
+                auth = aiohttp.BasicAuth(login=user, password=password)
+                async with aiohttp.ClientSession(timeout=ClientTimeout(20), auth=auth) as client:
+                    try:
+                        print('[+] Connecting to {}:{}'.format(router.address, router.port))
+                        async with client.request('GET', uri, ssl=context) as response:
+                            if response.status == 200:
+                                router.valid_credentials.append((user, password))
+                                logger.info(
+                                    '[+] Default credential worked for router {}:{}'.format(router.address,
+                                                                                            router.port))
+                            elif response.status == 401:
+                                logger.info(
+                                    '[-] Default credential did not work for router {}:{}'.format(router.address,
+                                                                                                  router.port))
+                            router.alive = True
+                    except TimeoutError:
+                        router.alive = False
+                        logger.warning(
+                            '[-] Unsuccessful connection to router {}:{}'.format(router.address, router.port))
+                    except ConnectionRefusedError:
+                        logger.warning(
+                            '[-] Unsuccessful connection to router {}:{}'.format(router.address, router.port))
                         router.alive = True
-                except:
-                    router.alive = False
+                    except Exception:
+                        logger.exception(
+                            '[-] Unsuccessful connection to router {}:{}'.format(router.address, router.port))
+                        router.alive = False
 
     def check_router_credentials(self, routers: List[BaseIndustrialRouter]):
         async def check_router_credentials_aio(routers):
+            semaphore = Semaphore(10)
             futures = []
             for router in routers:
-                futures.append(asyncio.ensure_future(self.check_default_password(router)))
-            await asyncio.wait(futures, return_when=ALL_COMPLETED)
+                if isinstance(router, self.__class__.router_cls):
+                    futures.append(asyncio.ensure_future(self.check_default_password(router, semaphore=semaphore)))
 
-        asyncio.get_event_loop().run_until_complete(check_router_credentials_aio(routers))
+            await asyncio.wait(futures, return_when=ALL_COMPLETED, )
+
+        asyncio.run(check_router_credentials_aio(routers), debug=True)
+
+    def get_address_info(self, routers):
+        for router in routers:
+            if isinstance(router, self.__class__.router_cls):
+                try:
+                    whois = IPWhois(router.address)
+                    results = whois.lookup_rdap(depth=1)
+                    if results['asn_country_code']:
+                        router.country = results['asn_country_code']
+                    if results['asn_description']:
+                        router.asn_description = results['asn_description']
+                except:
+                    pass
 
 
 class WestermoScanner(BaseIndustrialRouterScanner):
@@ -105,12 +185,88 @@ class WestermoScanner(BaseIndustrialRouterScanner):
 
 class MoxaScanner(BaseIndustrialRouterScanner):
     possible_headers = {'Server': ['MoxaHttp', 'MoxaHttp/1.0', 'MoxaHttp/2.2']}
-    default_credentials = [('admin', 'root'), ('', 'root'), ('', ''), ('admin', 'admin'), ('admin', '')]
+    default_credentials_http1 = [('root', 'efa59ad49b7bc93a9a7bb1004f24b1cc'),  # Root
+                                 ('', 'd41d8cd98f00b204e9800998ecf8427e'),  # Empty
+                                 ('admin', 'd8a1dd02029af4e10b495bc3ab03859e')]  # Admin
+
+    default_credentials_http2 = [('admin', 'root', '63a9f0ea7bb98050796b649e85481845'),  # Root
+                                 ('', 'root', '63a9f0ea7bb98050796b649e85481845'),  # Root
+                                 ('admin', 'admin', '21232f297a57a5a743894a0e4a801fc3'),  # Admin
+                                 ('admin', '', 'd41d8cd98f00b204e9800998ecf8427e')]  # Empty
+    valid_login_text = 'FRAME name="main" src="main.htm"'
     router_cls = MoxaRouter
+
+    def get_challenge_moxahttp_1_0(self, text):
+        regexp = 'set\(\"FakeChallenge\",\"(?P<challenge>[A-Z0-9]+)\"\)\;'
+        match = re.search(regexp, text.rstrip())
+        if match:
+            try:
+                return match.group('challenge')
+            except IndexError:
+                return None
+        else:
+            return None
+
+    def get_challenge_moxahttp_2_2(self, text):
+        regexp = '<INPUT type=hidden name=FakeChallenge value=(?P<challenge>[A-Z0-9]+)>'
+        match = re.search(regexp, text)
+        if match:
+            try:
+                return match.group('challenge')
+            except IndexError:
+                return None
+        else:
+            return None
+
+    @classmethod
+    async def check_default_password(cls, router, semaphore=Semaphore()):
+        uri = '{}://{}:{}/{}'.format(router.protocol, router.address, router.port, cls.url_path)
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+        context.options &= ~ssl.OP_NO_SSLv3
+        context.set_ciphers('HIGH:!DH:!aNULL')
+        async with semaphore:
+            async with aiohttp.ClientSession(timeout=ClientTimeout(20)) as client:
+                async with client.request('GET', uri, ssl=context) as response:
+                    router.alive = True
+                    if cls.valid_login_text in response.text:
+                        router.valid_credentials.append(('admin', 'no password'))
+                    else:
+                        if response.headers.get('Server') == 'MoxaHttp/1.0':
+                            await cls.check_password_moxahttp_1_0(client, context, response, router)
+                        elif response.headers.get('Server') == 'MoxaHttp/2.2':
+                            await cls.check_password_moxahttp_2_2(client, response, router)
+
+    @classmethod
+    async def check_password_moxahttp_1_0(cls, client, context, response, router):
+        challenge = cls.get_challenge_moxahttp_1_0(response.text)
+        for clear_password, password in cls.default_credentials_http1:
+            uri = '{}://{}:{}/home.htm?Password={}&Submit=Submit&token_text=&FakeChallenge={}' \
+                .format(router.protocol, router.address, router.port, password, challenge)
+            async with client.request('GET', uri, ssl=context) as response:
+                if cls.valid_login_text in response.text:
+                    router.valid_credentials.append(clear_password)
+
+    @classmethod
+    async def check_password_moxahttp_2_2(cls, client, context, response, router):
+        uri = '{}://{}:{}/'.format(router.protocol, router.address, router.port)
+        challenge = cls.get_challenge_moxahttp_2_2(response.text)
+        for user, clear_password, password in cls.default_credentials_http2:
+            payload = {
+                'Username': user,
+                'MD5Password': password,
+                'FakeChallenge': challenge,
+                'Submit.x': random.randint(0, 50),
+                'Submit.y': random.randint(0, 50)
+            }
+            async with client.post(uri, data=payload, ssl=context) as response:
+                if cls.valid_login_text in response.text:
+                    router.valid_credentials.append((user, clear_password))
 
 
 class EWonScanner(BaseIndustrialRouterScanner):
-    possible_headers = [{'Server': ['eWON']}]
+    possible_headers = {'Server': ['eWON']}
     default_credentials = [('adm', 'adm',)]
     router_cls = EWonRouter
     url_path = 'Ast/MainAst.shtm'
@@ -135,11 +291,18 @@ class IndustrialRouterAdapter(RobotAdapter):
     def scan_pipe_main(self):
         pass
 
+    def scan_network(self):
+        for scanner in self.router_scanners:
+            self.routers = scanner.check_routers(self.host_list, self.ports)
+            scanner.check_router_credentials(self.routers)
+
     def print_results(self):
         for router in self.routers:
             print(Fore.Green + 'Name' + router.name + Fore.RESET)
             print('\tAddress: {}:{}'.format(router.address, router.port))
             print('\tProtocol: ' + router.protocol)
+            print('\tCountry: ' + router.country)
+            print('\tASN Description: ' + router.asn_description)
             if router.alive:
                 print('\t' + Fore.GREEN + 'Alive' + Fore.RESET)
                 if len(router.valid_credentials):
@@ -150,16 +313,26 @@ class IndustrialRouterAdapter(RobotAdapter):
                 print('\t' + Fore.RED + 'Unreachable' + Fore.RESET)
 
     def write_to_file(self, out_file):
-        header = 'Type;Address;Port;Protocol;Valid Credentials\n'
+        header = 'Type;Address;Port;Protocol;Alive;Country;ASN Description;Valid Credentials\n'
         with open(out_file, 'w') as file:
             file.write(header)
             for router in self.routers:
-                line = '{};{};{};{};{}\n'.format(router.name, router.address, router.port,
-                                                 router.protocol, router.valid_credentials)
+                line = '{};{};{};{};{};{};{};{}\n'.format(router.name, router.address, router.port,
+                                                          router.protocol, router.alive, router.country,
+                                                          router.asn_description, router.valid_credentials)
                 file.write(line)
 
     def scan(self):
         if self.use_shodan:
             self.initialize_shodan()
             for scanner in self.router_scanners:
-                self.routers.append(scanner.check_routers_shodan(self.shodan_conn))
+                self.routers = self.routers + scanner.check_routers_shodan(self.shodan_conn)
+                scanner.check_router_credentials(self.routers)
+                # scanner.get_address_info(self.routers)
+        else:
+            for scanner in self.router_scanners:
+                for address in self.host_list:
+                    for port in self.ports:
+                        self.routers.append(scanner.check_routers(address, port))
+                        scanner.check_router_credentials(self.routers)
+                # scanner.get_address_info(self.routers)
