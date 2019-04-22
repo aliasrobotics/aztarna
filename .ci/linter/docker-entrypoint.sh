d7432cc9e152886c6bb1808989ab6a4bc92e6193
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-04-22 20:54:13 +0200

    Update docker-entrypoint.sh
    Fix typo

--- .ci/linter/docker-entrypoint.sh
+++ .ci/linter/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 #Install aztarna dependencies
 cd /root/aztarna
 python3.7 -m pip install -r requirements.txt
-python3.7 -m pip instsall pylint
+python3.7 -m pip install pylint
 python3.7 -m pylint linter/.pylintrc aztarna
