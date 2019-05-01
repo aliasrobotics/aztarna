a0a928e6b069ff1bb31d7c04047680c9e2f65d38
LanderU <lander.usategui@gmail.com>
2019-05-01 11:34:15 +0200

    docker-entrypoint: Change linter version

--- .ci/linter/docker-entrypoint.sh
+++ .ci/linter/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 #Install aztarna dependencies
 cd /root/aztarna
 python3.7 -m pip install -r requirements.txt
-python3.7 -m pip install pylint
-python3.7 -m pylint linter/.pylintrc aztarna
+pip3 install pylint
+pylint linter/.pylintrc aztarna
